b1e7f1048d4825c66bbdbe9e6b7d3679a819f56e
HabtB <email.Habtamu@gmail.com>
2022-09-08 18:15:35 +0000

    4-puts added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-function: Entry point
+ *
+ * Return:always returns 0 always
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
