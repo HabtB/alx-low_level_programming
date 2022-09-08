0ae49ccc7ba70493c87d62514b25ea12d9c63962
HabtB <email.Habtamu@gmail.com>
2022-09-08 18:20:39 +0000

    5-printf file added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-function: Entry point
+ *
+ * Return:always returns 0 always
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
