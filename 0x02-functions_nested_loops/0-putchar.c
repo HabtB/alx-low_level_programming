a4e8888b52553795c0ac45d9dcdaa4efe414d231
HabtB <email.Habtamu@gmail.com>
2022-09-13 22:12:10 +0000

    putchar added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main- prints the string '_putchar'
@@ -7,14 +6,14 @@
  */
 int main(void)
 {
-	char word[8] = "_putchar";
-
-	{
-		int i = 0;
-
-		for (; i < 8; i++)
-			_putchar(word[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
