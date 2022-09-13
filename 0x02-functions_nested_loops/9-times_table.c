cfcdd99631c0960730be207b5fb58d5ecf0ba148
HabtB <email.Habtamu@gmail.com>
2022-09-13 23:02:28 +0000

    9-recorrected and pushed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 			if ((x * y) / 10 >= 1)
 				_putchar(((x * y) / 10) + '0');
 
-			else if (x >= 0)
+			else if (x >= 0 && y != 0)
 				_putchar(' ');
 
 			_putchar(((x * y) % 10) + '0');
