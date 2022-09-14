00a76e0c513947cb34f87a3bcac41f7b00b51fc4
HabtB <email.Habtamu@gmail.com>
2022-09-14 13:09:15 +0000

    103-fibonacci added

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- prints all the fibonacci numbers until 50
+ *
+ * Return: returns zero upon Success
+ */
+
+int main(void)
+{
+	long int i = 1;
+	long int j = 2;
+	long sum = 2;
+
+	while (i < 4000000)
+	{
+		long int k;
+
+		k = i;
+		i = j;
+		j = k + j;
+
+		if (i % 2 == 0)
+			sum = sum + i;
+	}
+	printf("%ld", sum);
+	printf("\n");
+	return (0);
+}
