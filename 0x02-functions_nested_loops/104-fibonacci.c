c421ec29025b8e7ae2c20cd7e53e1edb17303e19
HabtB <email.Habtamu@gmail.com>
2022-09-14 13:56:35 +0000

    the last fibonacci.c added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - first 98 fibonatchi numbers
+ *
+ * Return: void.
+ */
+int main(void)
+{
+	unsigned long int a, a0, a1, b, b0, b1, c, c0, c1;
+	int m, n;
+
+	a = 1;
+	b = 2;
+	c = a + b;
+
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+	for (m = 3; m < 89; m++)
+	{
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	b0 = b / 1000000000;
+	b1 = b % 1000000000;
+	c0 = c / 1000000000;
+	c1 = c % 1000000000;
+	for (n = 89; n < 98; n++)
+	{
+		printf("%lu%lu, ", c0, c1);
+		a0 = b0;
+		a1 = b1;
+		b0 = c0;
+		b1 = c1;
+		c0 = c0 + c0 + ((a1 + b1) / 1000000000);
+		c1 = (a1 + b1) % 1000000000;
+	}
+printf("%lu%lu\n", c0, c1);
+	return (0);
+}
