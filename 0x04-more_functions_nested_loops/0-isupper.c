4a6f770c489123059d567084ef463b2b9d8b9cdc
HabtB <email.Habtamu@gmail.com>
2022-09-15 17:32:07 +0000

    0-isupper.c and README.md files are added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper- changes all the alphabets into lower case
+ *@c : integer value
+ * Return: always returns zero
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
