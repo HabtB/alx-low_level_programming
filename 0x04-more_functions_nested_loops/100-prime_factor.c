550f7d625edd6dba8c2fd7114aa1791c8e7a4fd0
HabtB <email.Habtamu@gmail.com>
2022-09-15 21:03:42 +0000

    100_prime_factor.c added

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds the largest prime factor of a number
+ *
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	double n = 612852475143;
+	double m = 612852475143;
+	int i = 2;
+	int imax = 1;
+
+	while (i <= (int)(sqrt(n)))
+	{
+		if ((int) n % i == 0)
+		{
+			imax = i * imax;
+			n = (double) (n / i);
+			i = 2;
+		}
+		else
+			i++;
+	}
+	int largest_pm;
+
+	largest_pm = m / imax;
+
+	printf("%d\n", largest_pm);
+}
