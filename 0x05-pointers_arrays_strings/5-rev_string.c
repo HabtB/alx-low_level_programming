1b1442f41d25766f7c53c72fa0db27110a592540
HabtB <email.Habtamu@gmail.com>
2022-09-19 21:46:56 +0000

    pointers files added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * rev_string- reverses a string
+ * @s: is a pointer to a string
+ *
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+
+	int i = 0;
+	int j = 0;
+	int len;
+	char rev[1000];
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		rev[j] = s[i];
+		j++;
+	}
+
+	for (i = 0; i <= len - 1; i++)
+	{
+		s[i] = rev[i];
+	}
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * puts2- prints one char out of two.
+ * @s: is a pointer to a string
+ *
+ * Return: none
+ */
+
+void puts2(char *s)
+{
+
+	int i = 0;
+	int j = 0;
+
+
+	while (s[i] != 0)
+	{
+		i++;
+		j++;
+	}
+
+	for (i = 0; i <= j - 1; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
