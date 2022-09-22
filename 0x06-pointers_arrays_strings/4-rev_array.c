e36bd2c88263ef53736c986d905ae2e258b281cf
HabtB <email.Habtamu@gmail.com>
2022-09-22 15:58:13 +0000

    files 3 to 7 are added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array- reverses the array
+ * @a: is a pointer to an integer
+ * @n: is the value of an integer
+ *
+ * Return: returns nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+
+	int j = 0;
+	int i = n - 1;
+
+	while (j < n / 2)
+	{
+		int tmp;
+		tmp = a[j];
+		a[j] = a[i];
+		a[i] = tmp;
+		i--;
+		j++;
+	}
+
+}
