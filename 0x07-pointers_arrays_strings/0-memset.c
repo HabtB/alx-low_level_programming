81b34aa163a98be68dbc3b77405175e3ca36bb53
HabtB <email.Habtamu@gmail.com>
2022-09-26 21:58:30 +0000

    0 to 7 files added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * _memset- fills the memory with a chosen constant
+ * @s: pointer to a character
+ * @b: byte chosen to fill
+ * @n: the length of the characters to be filled
+ *
+ * Return: returns the pointer to the character
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * _memcpy- fills the memory with a chosen constant
+ * @dest: pointer to a character
+ * @src: byte chosen to fill
+ * @n: the length of the characters to be filled
+ *
+ * Return: returns the pointer to copied character
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+
+	}
+	return (dest);
+}
