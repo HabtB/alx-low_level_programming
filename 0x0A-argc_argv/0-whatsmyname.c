612d9315d72cf09636822a39245815e523e92b83
HabtB <email.Habtamu@gmail.com>
2022-09-30 19:12:27 +0000

    argc files posted

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- prints out the name of the program
+ * @argc: number of arguments
+ * @argv: array of pointers to string arguements
+ *
+ * Return: always 0 for success
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void) argc;
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- prints the number of arguments passed
+ * @argc: number of arguments
+ * @argv: array of pointers to string arguements
+ *
+ * Return: always 0 for success
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void) argv;
+
+	return (0);
+}
