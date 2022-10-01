652740b46c954954674e399e3f70bc04ff341542
HabtB <email.Habtamu@gmail.com>
2022-10-01 00:39:52 +0000

    100-change.c modified to to consider negative numbers

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,34 +10,44 @@
 
 int main(int argc, char **argv)
 {
-	int coins = 0;
-	int amount = atoi(argv[1]);
-
 	if (argc == 1)
+	{
 		printf("%s\n", "Error");
+		return (1);
+	}
+	else if (atoi(argv[1]) <= 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	else
+	{
+		int coins = 0;
+		int amount = atoi(argv[1]);
 
-	if (amount >= 25)
-	{
-		coins += amount / 25;
-		amount %= 25;
+		if (amount >= 25)
+		{
+			coins += amount / 25;
+			amount %= 25;
+		}
+		if (amount >= 10)
+		{
+			coins += amount / 10;
+			amount %= 10;
+		}
+		if (amount >= 5)
+		{
+			coins += amount / 5;
+			amount %= 5;
+		}
+		if (amount >= 2)
+		{
+			coins += amount / 2;
+			amount %= 2;
+		}
+		else if (amount > 0)
+			coins += amount;
+		printf("%d\n", coins);
+		return (0);
 	}
-	if (amount >= 10)
-	{
-		coins += amount / 10;
-		amount %= 10;
-	}
-	if (amount >= 5)
-	{
-		coins += amount / 5;
-		amount %= 5;
-	}
-	if (amount >= 2)
-	{
-		coins += amount / 2;
-		amount %= 2;
-	}
-
-	coins += amount;
-	printf("%d\n", coins);
-	return (0);
 }
