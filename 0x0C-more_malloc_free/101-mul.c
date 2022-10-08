9cce9c273b7aa65fd1abb62ef207ed42fc5484cf
HabtB <email.Habtamu@gmail.com>
2022-10-08 03:25:15 +0000

    101-mul.c added

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,6 +20,16 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (*argv[1] < 47 || *argv[1] > 57)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[2]) < 48 || (*argv[2] > 57))
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	if (*argv[1] == 48)
 		num1 = 0;
 	else
