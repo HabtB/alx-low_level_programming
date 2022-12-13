943c0962346130623144f12229c540e12dffa482
HabtB <email.Habtamu@gmail.com>
2022-12-13 10:43:55 +0000

    101 modified

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/film.so https://wwww.github.com/Holbertonschool/0x18.c/
-export LD_PRELOAD=/tmp/film.so
+wget -P /tmp https://github.com/HabtB/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libhabt.so
+export LD_PRELOAD=/tmp/libhabt.so
