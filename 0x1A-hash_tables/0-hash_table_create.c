8432aeae2a7341ef4eacda2f1ff77c66344483e1
HabtB <email.Habtamu@gmail.com>
2023-01-07 00:35:02 +0000

    0 updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
@@ -19,8 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = calloc(sizeof(hash_node_t *), size);
 	if (hash_table->array == NULL)
 		return (NULL);
-	int i;
-
+	
 	for (i = 0; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
 
