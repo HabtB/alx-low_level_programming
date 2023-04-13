6398e95d5be80981a20d41e9a51345408783c6b2
HabtB <email.Habtamu@gmail.com>
2023-04-13 09:48:40 +0000

    0-linear updated

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,19 +11,18 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
-	int position = -1;
-
+	
 	if (!array || size == 0)
-		return (position);
+		return (-1);
 
 	for (; i < size; i++)
 	{
 		printf("value checked array [%li] = [%d] \n", i, array[i]);
 
 		if (array[i] == value)
-			return ((int)i);
+			return (i);
 	}
-	return (position);
+	return (-1);
 }
 
 
